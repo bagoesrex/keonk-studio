5382e361977136785e08605111ed65ea3a1fca3c
bagoesrex <bagoesrex@gmail.com>
2025-12-22 22:25:10 +0700

    chore: setup shadcn components and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { Card, CardHeader, CardTitle } from "./components/ui/card"
 
 export default function App() {
 
   return (
     <>
       <h1 className="text-4xl italic text-orange-500 font-bold">Keonk Tailwind :D</h1>
+      <Card >
+        <CardHeader>
+          <CardTitle>Hello</CardTitle>
+        </CardHeader>
+      </Card>
     </>
   )
 }
